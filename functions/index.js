71a875a81cdc457e3c68830b6cafbe68a6b359e0
Unknown <kennethjiepadasas@gmail.com>
2018-12-05 15:00:33 +0800

    register teacher account and show first time password

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp();
 const firestore = admin.firestore();
 
@@ -10,47 +10,79 @@ const firestore = admin.firestore();
 //  response.send("Hello from Firebase!");
 // });
 
-
 exports.createUser = functions.firestore
-.document('tempCreateUsers/{userEmail}').onWrite((change, context) => {
-  // ... Your code here
-  console.log(change.after.data());
-  let email = change.after.data().email;
-  let userSchoolId = change.after.data().userSchoolId
-  let password = change.after.data().password;
+  .document("tempCreateUsers/{userEmail}")
+  .onWrite((change, context) => {
+    // ... Your code here
+    console.log(change.after.data());
+    let email = change.after.data().email;
+    let password = change.after.data().password;
 
-  return admin.auth().createUser({
+    return admin.auth().createUser({
       email: email,
       emailVerified: true,
       password: password,
       disabled: false
-  })
-});
+    });
+  });
 
-exports.afterCreateUser = functions.auth.user().onCreate((user) => {
-    // ...
-    var userId = user.uid;
-    var email = user.email;
-    console.log("user Id: "+ userId+ " userEmail: "+email);
-    
-    return firestore.collection("tempCreateUsers").doc(email)
-    .onSnapshot((querySnapshot)=>{
-        firestore.collection("users").doc(userId).set({
-                email:email,
-                userId:userId,
-                userSchoolId:querySnapshot.data().userSchoolId,
-                timestamp: admin.firestore.FieldValue.serverTimestamp(),
-                userType:"teacher"
-            })
-        firestore.collection("teacherProfile").doc(userId).set({
-            accountStatus:"block",
-            userId:userId,
-            teacherId:querySnapshot.data().userSchoolId,
-            timestamp:admin.firestore.FieldValue.serverTimestamp(),
-            teacherName:""
+exports.afterCreateUser = functions.auth.user().onCreate(user => {
+  // ...
+  var userId = user.uid;
+  var email = user.email;
+  console.log("user Id: " + userId + " userEmail: " + email);
+
+  return firestore
+    .collection("tempCreateUsers")
+    .doc(email)
+    .onSnapshot(querySnapshot => {
+      firestore
+        .collection("users")
+        .doc(userId)
+        .set({
+          email: email,
+          userId: userId,
+          userName: querySnapshot.data().userName,
+          userSchoolId: querySnapshot.data().userSchoolId,
+          userType: querySnapshot.data().userType,
+          userImage:
+            "https://firebasestorage.googleapis.com/v0/b/classrecordsystem-f6067.appspot.com/o/assets%2Favatar-1577909_960_720.png?alt=media&token=ff2ede2c-d86a-481e-87e5-384011a368ca"
         })
-    })
-  
+     
+         
+
+
+      if(querySnapshot.data().userType == "teacher"){
+          console.log({
+              accountStatus:"active",
+              userId:userId,
+              teacherId:querySnapshot.data().userSchoolId,
+              teacherName:querySnapshot.data().userName,
+          })
+          firestore
+          .collection("teacherProfile")
+          .doc(userId)
+          .set({
+            accountStatus: "active",
+            userId: userId,
+            teacherId: querySnapshot.data().userSchoolId,
+            teacherName: querySnapshot.data().userName
+          });
+      }
+    });
 });
 
-
+// exports.createUser = functions.firestore
+//   .document("users/{userEmail}")
+//   .onWrite((change, context) => {
+//     // ... Your code here
+//     return firestore
+//       .collection("teacherProfile")
+//       .doc(userId)
+//       .set({
+//         accountStatus: "block",
+//         userId: change.after.data().userId,
+//         teacherId: change.after.data().userSchoolId,
+//         teacherName: change.after.data().userName
+//       });
+//   });
--- public/teachers.js
+++ public/teachers.js
@@ -33,7 +33,12 @@ class ManageTeachers extends React.Component {
           <h1>Manage Teachers</h1>
         </div>
         <div className="row">
-          <button type="button" className="btn btn-dark" data-toggle="modal" data-target="#addInstructorModal">
+          <button
+            type="button"
+            className="btn btn-dark"
+            data-toggle="modal"
+            data-target="#addInstructorModal"
+          >
             Register Instructor
           </button>
         </div>
@@ -41,101 +46,175 @@ class ManageTeachers extends React.Component {
           <div className="group-list w-100">
             <div className="list-group-item border-0">
               <div className="row">
-                <div className="col font-weight-bold">Teacher Name</div>
-                <div className="col font-weight-bold">
-                  <div className="d-flex flex-row-reverse pr-5">
-                    Account Status
-                  </div>
+                <div className="col-1" />
+                <div className="col-1 font-weight-bold">ID</div>
+                <div className="col font-weight-bold">Instructor Name</div>
+                <div className="col font-weight-bold">Email</div>
+                <div className="col font-weight-bold">Password</div>
+                <div className="col-1 font-weight-bold">
+                  <div className="d-flex">Status</div>
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div className="row">
-          <div className="group-list w-100" id="teacherListContainer" />
+          <div className="group-list p-2 w-100" id="teacherListContainer" />
         </div>
         {/* add Instructor modal herer */}
-        <Modal id = {"addInstructorModal"} content = {<InputInstructorModalContent/>} modalTitle = {"Add Instructor"}/>
+        <Modal
+          id={"addInstructorModal"}
+          content={
+            <InputInstructorModalContent
+              id="addInstructorModal"
+              context={this}
+            />
+          }
+          modalTitle={"Add Instructor"}
+        />
       </React.Fragment>
     );
   }
 }
 
 class Modal extends React.Component {
-  state = { data:{} }
-  render() { 
-    return ( 
+  state = {
+    data: { email: "test", instructorId: "11", instructorName: "name" }
+  };
+  render() {
+    return (
       <div
-      className="modal fade"
-      id={this.props.id}
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div className="modal-dialog" role="document">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalLabel">
-             {this.props.modalTitle}
-            </h5>
-            <button
-              type="button"
-              className="close"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div className="modal-body">
+        className="modal fade"
+        id={this.props.id}
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                {this.props.modalTitle}
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+
             {/* modal Content */}
             {this.props.content}
           </div>
-          <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-secondary"
-              data-dismiss="modal"
-            >
-              Close
-            </button>
-            <button type="button" className="btn btn-primary">
-              Save changes
-            </button>
-          </div>
         </div>
       </div>
-    </div>
-
-     );
+    );
   }
 }
 
 class InputInstructorModalContent extends React.Component {
-  state = {  }
-  render() { 
-    return ( 
+  state = {
+    email: "",
+    userSchoolId: "",
+    userName: "",
+    userType: "teacher"
+  };
+  saveInStructor = () => {
+    let sup = this;
+    let timeStamp = db.collection("tempCreateUsers").doc().id;
+    db.collection("tempCreateUsers")
+      .doc(sup.state.email)
+      .set({ ...this.state, password: timeStamp })
+      .then(() => {
+        $("#" + this.props.id).modal("hide");
+      });
+  };
+
+  render() {
+    return (
       <React.Fragment>
-         <div className="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+        <div className="modal-body">
+          <div className="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+              onChange={text => {
+                this.setState({
+                  email: text.target.value
+                });
+              }}
+              defaultValue={this.state.email}
+            />
+          </div>
+          <div className="form-group">
+            <label for="exampleInputEmail1">Instructor ID</label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter Instructor ID"
+              onChange={text => {
+                this.setState({
+                  userSchoolId: text.target.value
+                });
+              }}
+              defaultValue={this.state.teacherId}
+            />
+          </div>
+          <div className="form-group">
+            <label for="exampleInputEmail1">Instructor Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter Instructor Name"
+              onChange={text => {
+                this.setState({
+                  userName: text.target.value
+                });
+              }}
+              defaultValue={this.state.teacherName}
+            />
+          </div>
         </div>
-        <div className="form-group">
-          <label for="exampleInputEmail1">Instructor ID</label>
-          <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Instructor ID"/>
-        </div>
-        <div className="form-group">
-          <label for="exampleInputEmail1">Instructor Name</label>
-          <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Instructor Name"/>
+        <div className="modal-footer">
+          <button
+            type="button"
+            className="btn btn-secondary"
+            data-dismiss="modal"
+          >
+            Close
+          </button>
+          <button
+            type="button"
+            onClick={() => this.saveInStructor()}
+            className="btn btn-primary"
+          >
+            Save changes
+          </button>
         </div>
       </React.Fragment>
-     );
+    );
   }
 }
 
 class TeacherItem extends React.Component {
-  state = { extendView: "d-none", profile: "" };
+  state = {
+    extendView: "d-none",
+    profile: "",
+    passwordPlaceholder: "********************",
+    showPassword: false
+  };
   setStatus() {
     db.collection("teacherProfile")
       .doc(this.props.id)
@@ -144,6 +223,16 @@ class TeacherItem extends React.Component {
         this.props.accountStatus == "active" ? "pending" : "active"
       );
   }
+  getPassword = () => {
+    let sup = this;
+    db.collection("tempCreateUsers")
+      .doc(sup.state.profile.email)
+      .onSnapshot(snapshot => {
+        sup.setState({
+          password: snapshot.data().password
+        });
+      });
+  };
   setToActive() {
     db.collection("teacherProfile")
       .doc(this.props.id)
@@ -168,6 +257,7 @@ class TeacherItem extends React.Component {
         sup.setState({
           profile: doc.data()
         });
+        sup.getPassword();
       });
   }
   togleExtend() {
@@ -201,6 +291,18 @@ class TeacherItem extends React.Component {
         );
       });
   }
+  copyToClipboard =()=>{
+    let copyText = document.getElementById("tempPassword");
+
+    /* Select the text field */
+    copyText.select();
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Copied the Password: " + copyText.value);
+  }
   componentDidMount() {
     this.getSubjectList();
     this.getTeacherUserDetails();
@@ -208,20 +310,73 @@ class TeacherItem extends React.Component {
   render() {
     return (
       <div className="list-group-item mt-1 border-0 bg-light">
-        <div className="row" onClick={this.togleExtend.bind(this)}>
-          <div className="col">
-            <div className="row font-weight-bold">{this.props.teacherName}</div>
+        <div className="row d-flex align-items-center pl-2">
+          <div className="col-1">
+            <div className="row h-100 d-flex align-items-center">
+              <div className="col d-flex align-items-center">
+                <i
+                  class="ml-2 material-icons text-muted"
+                  onClick={this.togleExtend.bind(this)}
+                >
+                  arrow_drop_down_circle
+                </i>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-1">
             <div className="row">
               <small className="text-primary">{this.props.teacherId}</small>
             </div>
+          </div>
+          <div className="col">
+            <div className="row font-weight-bold">{this.props.teacherName}</div>
+          </div>
+          <div className="col">
             <div className="row">{this.state.profile.email}</div>
           </div>
           <div className="col">
-            <div className="d-flex flex-row-reverse pr-5">
+            <div className="row h-100 d-flex align-items-center">
+              <div className="col h-100 d-flex align-items-center">
+                <input
+                  type={this.state.showPassword ? "text" : "password"}
+                  defaultValue={this.state.password}
+                  class="form-control form-control-sm border-0 font-weight-bold text-muted"
+                  id="tempPassword"
+                  placeholder="Enter email"
+                />
+                {/* <small className = "font-weight-bold">
+                  {" "}
+                  {!this.state.showPassword
+                    ? this.state.passwordPlaceholder
+                    : this.state.password}{" "}
+                </small> */}
+              </div>
+              <div className="col-auto">
+                <i
+                  onClick={() => {
+                    this.setState({ showPassword: !this.state.showPassword });
+                  }}
+                  class="material-icons small-icn text-muted"
+                >
+                  {this.state.showPassword ? "visibility" : "visibility_off"}
+                </i>
+                {this.state.showPassword? <i class="ml-2 small-icn material-icons text-muted" onClick = {()=>{
+                  this.setState({ showPassword: !this.state.showPassword });
+                  this.copyToClipboard()
+                }}>
+                  file_copy
+                </i>:""}
+              </div>
+            </div>
+          </div>
+
+          <div className="col-1">
+            <div className="d-flex flex-row-reverse pr-3">
               <div class="dropdown">
                 <button
                   className={
-                    "btn dropdown-toggle  btn-" +
+                    "btn dropdown-toggle btn-sm  btn-" +
                     (this.props.accountStatus == "active"
                       ? "success"
                       : this.props.accountStatus == "pending"
